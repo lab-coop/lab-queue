6a41ac212b0a512a9dee33ed9a5e9d8e58e75caa
Erik Wyatt <kire321@gmail.com>
2017-02-03 13:48:37 +0100

    increase test verbosity in preparation for a memory implementation

--- features/steps/queue.js
+++ features/steps/queue.js
@@ -10,12 +10,12 @@ module.exports = function() {
 
   this.When('a random message pushed to queue "$queueName"', async function (queueName) {
     const queue = await container.queue;
-    assert((await queue.publish(queueName, true)));
+    assert((await queue.publish(queueName, true)), "Expected publish to return truthy");
   });
 
   this.When('a consumer is attached to queue "$queueName"', async function (queueName) {
     const queue = await container.queue;
-    assert((await queue.consume(queueName, () => {})));
+    assert((await queue.consume(queueName, () => {})), "Expected consume to return truthy");
   });
 
   this.When('"$queueName" contains $messageCount message', async function (queueName, messageCount) {
@@ -24,7 +24,7 @@ module.exports = function() {
     this.thrown = false;
     try {
       const checkResult = await queue.messageCount(queueName);
-      return assert(checkResult === messageCount);
+      return assert(checkResult === messageCount, "Expected checkResult to equal messageCount");
     } catch(err) {
       console.log(err);
       this.thrown = true;
@@ -37,22 +37,21 @@ module.exports = function() {
     this.thrown = false;
     try {
       const checkResult = await queue.consumerCount(queueName);
-      return assert(checkResult === consumerCount);
+      return assert(checkResult === consumerCount, "Expected checkResult to equal consumerCount");
     } catch(err) {
+      console.log(err);
       this.thrown = true;
     }
   });
 
   this.Then('the last operation thrown error', async function () {
     const queue = await container.queue;
-    assert(this.thrown === true);
+    assert(this.thrown === true, "Expected a thing to be thrown");
   });
 
   this.Then('the last operation didn\'t throw error', async function () {
     const queue = await container.queue;
-    assert(this.thrown === false);
+    assert(this.thrown === false, "Expected nothing to be thrown");
   });
 
 };
-
-
