e2b30854592c0be604f7c193625aed1483ad47e7
Denes Pal <denes.pal@lab.coop>
2017-02-10 21:55:23 +0100

    refactor: Implement cancel consumption for memory implementation
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- dist/implementations/memory/index.js
+++ dist/implementations/memory/index.js
@@ -1,152 +1,230 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _regenerator = require("babel-runtime/regenerator");
+var _regenerator = require('babel-runtime/regenerator');
 
 var _regenerator2 = _interopRequireDefault(_regenerator);
 
-var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
+exports.default = memoryQueueService;
+
+var _lodash = require('lodash');
+
+var _rr = require('rr');
+
+var _rr2 = _interopRequireDefault(_rr);
+
+var _shortid = require('shortid');
+
+var _shortid2 = _interopRequireDefault(_shortid);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var queues = {};
+function memoryQueueService(config) {
+  var dispatchMessages = function () {
+    var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee(queueName) {
+      return _regenerator2.default.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              if (!(consumerCount(queueName) && messageCount(queueName))) {
+                _context.next = 5;
+                break;
+              }
 
-var ensureQueue = function ensureQueue(queueName) {
-  return queues[queueName] || (queues[queueName] = {
-    messages: [],
-    consumers: []
+              _context.next = 3;
+              return dispatchOneMessage(queueName, chooseConsumer(queueName));
+
+            case 3:
+              _context.next = 0;
+              break;
+
+            case 5:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, _callee, this);
+    }));
+
+    return function dispatchMessages(_x) {
+      return _ref.apply(this, arguments);
+    };
+  }();
+
+  var dispatchOneMessage = function () {
+    var _ref2 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2(queueName, consumer) {
+      var message;
+      return _regenerator2.default.wrap(function _callee2$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              message = getFirstMessage(queueName);
+              _context2.prev = 1;
+              _context2.next = 4;
+              return consumer(message);
+
+            case 4:
+              _context2.next = 9;
+              break;
+
+            case 6:
+              _context2.prev = 6;
+              _context2.t0 = _context2['catch'](1);
+
+              queues[queueName].messages.push(message);
+
+            case 9:
+            case 'end':
+              return _context2.stop();
+          }
+        }
+      }, _callee2, this, [[1, 6]]);
+    }));
+
+    return function dispatchOneMessage(_x2, _x3) {
+      return _ref2.apply(this, arguments);
+    };
+  }();
+
+  var queues = {};
+  var consumersChanged = false;
+
+  return Object.freeze({
+    consume: consume,
+    publish: publish,
+    exists: exists,
+    remove: remove,
+    removeIfExists: removeIfExists,
+    purge: purge,
+    purgeIfExists: purgeIfExists,
+    messageCount: messageCount,
+    consumerCount: consumerCount
   });
-};
 
-var dispatchMessages = function dispatchMessages(queueName) {
-  if (queues[queueName].consumers.length) {
-    queues[queueName].messages.forEach(queues[queueName].consumers[0]);
+  function ensureQueue(queueName) {
+    if (!queues.hasOwnProperty(queueName)) {
+      queues[queueName] = {
+        messages: [],
+        consumers: {},
+        consumersArray: []
+      };
+    }
+  }
+
+  function exists(queueName) {
+    return queues.hasOwnProperty(queueName);
+  }
+
+  function remove(queueName) {
+    delete queues[queueName];
+  }
+
+  function removeIfExists(queueName) {
+    if (exists(queueName)) {
+      remove(queueName);
+    }
+  }
+
+  function purge(queueName) {
     queues[queueName].messages = [];
   }
-};
 
-exports.default = function (config) {
-  return {
-    removeIfExists: function () {
-      var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee(queueName) {
-        return _regenerator2.default.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                delete queues[queueName];
+  function purgeIfExists(queueName) {
+    if (exists(queueName)) {
+      purge(queueName);
+    }
+  }
 
-              case 1:
-              case "end":
-                return _context.stop();
-            }
-          }
-        }, _callee, undefined);
-      }));
+  function publish(queueName, message) {
+    ensureQueue(queueName);
+    pushMessage(queueName, message);
+    process.nextTick(function () {
+      return dispatchMessages(queueName);
+    });
+    return true;
+  }
 
-      function removeIfExists(_x) {
-        return _ref.apply(this, arguments);
-      }
+  function consume(queueName, consumer) {
+    ensureQueue(queueName);
+    var consumerId = addConsumer(queueName, consumer);
+    process.nextTick(function () {
+      return dispatchMessages(queueName);
+    });
+    return function () {
+      return removeConsumer(queueName, consumerId);
+    };
+  }
 
-      return removeIfExists;
-    }(),
-    publish: function () {
-      var _ref2 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2(queueName, message) {
-        return _regenerator2.default.wrap(function _callee2$(_context2) {
-          while (1) {
-            switch (_context2.prev = _context2.next) {
-              case 0:
-                ensureQueue(queueName);
-                queues[queueName].messages.push(message);
-                dispatchMessages(queueName);
-                return _context2.abrupt("return", true);
+  function addConsumer(queueName, consumer) {
+    var consumerId = _shortid2.default.generate();
+    queues[queueName].consumers[consumerId] = consumer;
+    consumersChanged = true;
+    return consumerId;
+  }
 
-              case 4:
-              case "end":
-                return _context2.stop();
-            }
-          }
-        }, _callee2, undefined);
-      }));
+  function removeConsumer(queueName, consumerId) {
+    delete queues[queueName].consumers[consumerId];
+    consumersChanged = true;
+  }
 
-      function publish(_x2, _x3) {
-        return _ref2.apply(this, arguments);
-      }
+  function chooseConsumer(queueName) {
+    if (consumersChanged) {
+      queues[queueName].consumersArray = Object.values(queues[queueName].consumers);
+    }
+    return (0, _rr2.default)(queues[queueName].consumersArray);
+  }
 
-      return publish;
-    }(),
-    messageCount: function () {
-      var _ref3 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee3(queueName) {
-        return _regenerator2.default.wrap(function _callee3$(_context3) {
-          while (1) {
-            switch (_context3.prev = _context3.next) {
-              case 0:
-                return _context3.abrupt("return", queues[queueName].messages.length);
+  function getFirstMessage(queueName) {
+    return queues[queueName].messages.shift();
+  }
 
-              case 1:
-              case "end":
-                return _context3.stop();
-            }
-          }
-        }, _callee3, undefined);
-      }));
+  function pushMessage(queueName, message) {
+    queues[queueName].messages.push(message);
+  }
 
-      function messageCount(_x4) {
-        return _ref3.apply(this, arguments);
-      }
+  function messageCount(queueName) {
+    return queues[queueName].messages.length;
+  }
 
-      return messageCount;
-    }(),
-    consume: function () {
-      var _ref4 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee4(queueName, handler) {
-        return _regenerator2.default.wrap(function _callee4$(_context4) {
-          while (1) {
-            switch (_context4.prev = _context4.next) {
-              case 0:
-                ensureQueue(queueName);
-                queues[queueName].consumers.push(handler);
-                dispatchMessages(queueName);
-                return _context4.abrupt("return", true);
+  function consumerCount(queueName) {
+    return Object.values(queues[queueName].consumers).length;
+  }
+}
 
-              case 4:
-              case "end":
-                return _context4.stop();
-            }
-          }
-        }, _callee4, undefined);
-      }));
-
-      function consume(_x5, _x6) {
-        return _ref4.apply(this, arguments);
-      }
-
-      return consume;
-    }(),
-    consumerCount: function () {
-      var _ref5 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee5(queueName) {
-        return _regenerator2.default.wrap(function _callee5$(_context5) {
-          while (1) {
-            switch (_context5.prev = _context5.next) {
-              case 0:
-                return _context5.abrupt("return", queues[queueName].consumers.length);
-
-              case 1:
-              case "end":
-                return _context5.stop();
-            }
-          }
-        }, _callee5, undefined);
-      }));
-
-      function consumerCount(_x7) {
-        return _ref5.apply(this, arguments);
-      }
-
-      return consumerCount;
-    }()
-  };
-};
+// export default (config) => ({
+//   removeIfExists: async (queueName) => {
+//     delete queues[queueName]
+//   },
+//   publish: async (queueName, message) => {
+//     ensureQueue(queueName)
+//     queues[queueName].messages.push(message)
+//     dispatchMessages(queueName)
+//     return true;
+//   },
+//   messageCount: async (queueName) => {
+//     return queues[queueName].messages.length
+//   },
+//   consume: async (queueName, handler) => {
+//     ensureQueue(queueName)
+//     queues[queueName].consumers.push(handler)
+//     return () => {
+//       console.log('CANCEL CALLED')
+//       queues[queueName].consumers = remove(queues[queueName].consumers, consumeHandler => {
+//         console.log(consumeHandler);
+//         console.log(handler);
+//         return consumeHandler == handler;
+//       });
+//       // console.log(queues[queueName].consumers);
+//     };
+//   },
+//   consumerCount: async (queueName) => {
+//     return queues[queueName].consumers.length
+//   }
+// })
--- implementations/memory/index.js
+++ implementations/memory/index.js
@@ -1,38 +1,119 @@
-const queues = {}
+import rr from 'rr';
+import shortid from 'shortid';
 
-const ensureQueue = (queueName) => queues[queueName] || (queues[queueName] = {
-  messages: [],
-  consumers: [],
-})
+export default function memoryQueueService(config) {
 
-const dispatchMessages = (queueName) => {
-  if(queues[queueName].consumers.length) {
-    queues[queueName].messages.forEach(queues[queueName].consumers[0])
-    queues[queueName].messages = []
+  const queues = {};
+  let consumersChanged = false;
+
+  return Object.freeze({
+    consume,
+    publish,
+    exists,
+    remove,
+    removeIfExists,
+    purge,
+    purgeIfExists,
+    messageCount,
+    consumerCount
+  });
+
+  function ensureQueue(queueName) {
+    if (!queues.hasOwnProperty(queueName)) {
+      queues[queueName] = {
+        messages: [],
+        consumers: {},
+        consumersArray: []
+      }
+    }
+  }
+
+  function exists(queueName) {
+    return queues.hasOwnProperty(queueName);
+  }
+
+  function remove(queueName) {
+    delete queues[queueName];
+  }
+
+  function removeIfExists(queueName) {
+    if (exists(queueName)) {
+      remove(queueName);
+    }
+  }
+
+  function purge(queueName) {
+    queues[queueName].messages = [];
+  }
+
+  function purgeIfExists(queueName) {
+    if (exists(queueName)) {
+      purge(queueName);
+    }
+  }
+
+  function publish(queueName, message) {
+    ensureQueue(queueName);
+    pushMessage(queueName, message);
+    process.nextTick(() => dispatchMessages(queueName));
+    return true;
+  }
+
+  function consume(queueName, consumer) {
+    ensureQueue(queueName);
+    const consumerId = addConsumer(queueName, consumer);
+    process.nextTick(() => dispatchMessages(queueName));
+    return () => removeConsumer(queueName, consumerId);
+  }
+
+  function addConsumer(queueName, consumer) {
+    const consumerId = shortid.generate();
+    queues[queueName].consumers[consumerId] = consumer;
+    consumersChanged = true;
+    return consumerId;
+  }
+
+  function removeConsumer(queueName, consumerId) {
+    delete queues[queueName].consumers[consumerId];
+    consumersChanged = true;
+  }
+
+  async function dispatchMessages(queueName) {
+    while (consumerCount(queueName) && messageCount(queueName)) {
+      await dispatchOneMessage(queueName, chooseConsumer(queueName));
+    }
+  }
+
+  function chooseConsumer(queueName) {
+    if (consumersChanged) {
+      queues[queueName].consumersArray = Object.values(queues[queueName].consumers)
+    }
+    return rr(queues[queueName].consumersArray);
+  }
+
+  async function dispatchOneMessage(queueName, consumer) {
+    const message = getFirstMessage(queueName);
+    try {
+      await consumer(message);
+    } catch (err) {
+      queues[queueName].messages.push(message);
+    }
+  }
+
+  function getFirstMessage(queueName) {
+    return queues[queueName].messages.shift()
+  }
+
+  function pushMessage(queueName, message) {
+    queues[queueName].messages.push(message);
+  }
+
+  function messageCount(queueName) {
+    return queues[queueName].messages.length;
+  }
+
+  function consumerCount(queueName) {
+    return Object.values(queues[queueName].consumers).length;
   }
 
 }
-
-export default (config) => ({
-  removeIfExists: async (queueName) => {
-    delete queues[queueName]
-  },
-  publish: async (queueName, message) => {
-    ensureQueue(queueName)
-    queues[queueName].messages.push(message)
-    dispatchMessages(queueName)
-    return true
-  },
-  messageCount: async (queueName) => {
-    return queues[queueName].messages.length
-  },
-  consume: async (queueName, handler) => {
-    ensureQueue(queueName)
-    queues[queueName].consumers.push(handler)
-    dispatchMessages(queueName)
-    return true
-  },
-  consumerCount: async (queueName) => {
-    return queues[queueName].consumers.length
-  }
-})
