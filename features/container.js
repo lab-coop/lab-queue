2121beb2d3b58fc1d82493a98a022d75c9f2cbe1
Denes Pal <denes.pal@lab.coop>
2017-02-21 16:41:23 +0100

    fix: Attach a logger service
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- features/container.js
+++ features/container.js
@@ -5,7 +5,17 @@ const config = function () {
   return require('config');
 };
 
+function logger() {
+  return () => ({
+    info: console.log,
+    error: console.log,
+    debug: console.log
+  })
+}
+
+
 di.service('config', config);
-di.service('queue', queue, 'config');
+di.service('logger', logger);
+di.service('queue', queue, 'config', 'logger');
 
 module.exports = di.container;
